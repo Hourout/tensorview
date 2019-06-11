3b8124d77580a50bccf72e877d911c197c3d0de5
JinQing Lee <hourout@163.com>
2019-06-11 18:54:49 +0800

    Create __init__.py

---
+++ tensorview/__init__.py
@@ -0,0 +1,6 @@
+from ._general_plot import plot_metrics
+from ._keras_callbacks_PlotMetricsOnBatch import PlotMetricsOnBatch
+from ._keras_callbacks_PlotMetricsOnEpoch import PlotMetricsOnEpoch
+
+__version__ = '0.1.0'
+__author__ = 'JinQing Lee'
