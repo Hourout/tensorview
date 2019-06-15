64dbe815bfa3228e063f0e3e6aa43a23c14b8997
JinQing Lee <hourout@163.com>
2019-06-15 15:11:45 +0800

    Update __init__.py

--- tensorview/__init__.py
+++ tensorview/__init__.py
@@ -1,6 +1,17 @@
-from ._general_plot import plot_metrics
-from ._keras_callbacks_PlotMetricsOnBatch import PlotMetricsOnBatch
-from ._keras_callbacks_PlotMetricsOnEpoch import PlotMetricsOnEpoch
+from tensorview import train
 
 __version__ = '0.1.0'
 __author__ = 'JinQing Lee'
+
+
+def _hello():
+    print("""
+------------------------------------------------------------------------------------
+      Linora
+--------------------
+      Version      : --  {}  --
+      Author       : --  {}  --
+      License      : Apache-2.0
+      Homepage     : https://github.com/Hourout/linora
+      Description  : Simple and efficient tools for data mining and data analysis.
+------------------------------------------------------------------------------------""".format(__version__, __author__))
