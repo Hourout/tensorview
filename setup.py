a4f2e4c91d574f80e1ef5074ee793c72100d7065
JinQing Lee <hourout@163.com>
2019-06-15 15:23:50 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ def readme():
     with open('README.md', encoding='utf-8') as f:
         return f.read()
 
-setup(name='beefly',
+setup(name='tensorview',
       version='0.1.0',
       install_requires=['matplotlib', 'tensorflow>=2.0.0b0'],
       description='Dynamic visualization training service in Jupyter Notebook for Keras tf.keras and others.',
       long_description=readme(),
       long_description_content_type='text/markdown',
-      url='https://github.com/Hourout/beefly',
+      url='https://github.com/Hourout/tensorview',
       author='JinQing Lee, Gaojie Wei',
       author_email='hourout@163.com',
       keywords=['keras-visualization', 'tensorflow-visualization', 'keras', 'tensorflow', 'tf.keras', 'plot', 'chart'],
