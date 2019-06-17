fd3c9fa184ea836b80cdb5f580e26aafb13cd384
JinQing Lee <hourout@163.com>
2019-06-17 13:41:15 +0800

    Update __init__.py

--- tensorview/train/__init__.py
+++ tensorview/train/__init__.py
@@ -1,3 +1,5 @@
 from tensorview.train._general_plot import plot_metrics
-# from tensorview.train._keras_callbacks_PlotMetricsOnBatch import PlotMetricsOnBatch
-# from tensorview.train._keras_callbacks_PlotMetricsOnEpoch import PlotMetricsOnEpoch
+from tensorview.train._keras_callbacks_PlotMetricsOnBatch import PlotMetricsOnBatch
+from tensorview.train._keras_callbacks_PlotMetricsOnEpoch import PlotMetricsOnEpoch
+
+__beefly_version__ = '0.8.0'
