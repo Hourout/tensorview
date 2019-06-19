d0f3acf2e9aa71fbf824db79b165cc4299d09d70
JinQing Lee <hourout@163.com>
2019-06-19 14:43:29 +0800

    Update _keras_callbacks_PlotMetricsOnBatch.py

--- tensorview/train/_keras_callbacks_PlotMetricsOnBatch.py
+++ tensorview/train/_keras_callbacks_PlotMetricsOnBatch.py
@@ -16,17 +16,42 @@ class PlotMetricsOnBatch(Callback):
     """
     Arguments:
         metrics_name : list, Customized evaluation indicator name list,
-                      sequentially created according to loss function and measurement function;
+                       sequentially created according to loss function and measurement function;
+        valid_x : Input data. It could be:
+                  A Numpy array (or array-like), or a list of arrays (in case the model has multiple inputs).
+                  A TensorFlow tensor, or a list of tensors (in case the model has multiple inputs).
+                  A dict mapping input names to the corresponding array/tensors, if the model has named inputs.
+                  A tf.data dataset or a dataset iterator.
+                  A generator or keras.utils.Sequence instance.
+        valid_y : Target data. Like the input data x, it could be either Numpy array(s) or TensorFlow tensor(s). 
+                  It should be consistent with x (you cannot have Numpy inputs and tensor targets, or inversely).
+                  If x is a dataset, dataset iterator, generator or keras.utils.Sequence instance,
+                  y should not be specified (since targets will be obtained from the iterator/dataset).
+        valid_batch_size : Integer or None. Number of samples per gradient update.
+                           If unspecified, batch_size will default to 32.
+                           Do not specify the batch_size is your data is in the form of symbolic tensors,
+                           dataset, dataset iterators, generators, or keras.utils.
+                           Sequence instances (since they generate batches).
+        valid_steps : Integer or None.
+                      Total number of steps (batches of samples) before declaring the evaluation round finished.
+                      Ignored with the default value of None.
+                      If x is a tf.data dataset or a dataset iterator, and steps is None,
+                      'evaluate' will run until the dataset is exhausted.
         columns : int, default 2，The number of sub graphs that the width of metrics
-                 visualiztion image to accommodate at most;
+                  visualiztion image to accommodate at most;
         iter_num : int, default None, Pre-specify the maximum value of x-axis in each
-                  sub-picture to indicate the maximum number of batch training;
+                   sub-picture to indicate the maximum number of batch training;
         wait_num : int, default 1, Indicates how many batches are drawn each time a graph is drawn;
         figsize : tuple，default None，Represents the customize image size;
         cell_size : tuple, default (6, 4)，Indicates the customize image size, which is used when figsize=None;
         valid_fmt : str, default "val_{}", The string preceding the underscore is used to
                    implement the validation set indicator naming;
         eval_batch_num : int, default None, Indicates how many batches are evaluated for each validation set;
+        visual_name : str, default 'model_visual', save plot image with html file name.
+        visual_name_gif : str, default 'model_visual_gif', save plot gif with html file name.
+        visual_path : str, default None, train end save last image or gif file with html format to path;
+        visual_image : bool, default True, if visual_image=False, train end not save image.
+        visual_gif : bool, default False, if save_gif=True, train end save all image to gif;
     """
     def __init__(self, metrics_name, valid_x=None, valid_y=None, valid_steps=None,
                  valid_batch_size=None, valid_sample_weight=None,
@@ -60,7 +85,6 @@ class PlotMetricsOnBatch(Callback):
             self.figsize = (self.columns*self.cell_size[0], ((len(self.metrics)+1)//self.columns+1)*self.cell_size[1])
         if self.eval_batch_num is not None:
             self.new_val_name = [self.valid_fmt.split('_')[0]+'_'+i for i in self.metrics_name]
-#             self.metrics = self.new_val_name+self.metrics_name
 
     def on_batch_end(self, batch, logs=None):
         self.batch_num += 1
@@ -69,17 +93,10 @@ class PlotMetricsOnBatch(Callback):
         for old_name, new_name in zip(self.model.metrics_names, self.metrics_name):
             logs[new_name] = logs.pop(old_name)
         if self.eval_batch_num is not None:
-#             self.new_val_name = [self.valid_fmt.split('_')[0]+'_'+i for i in self.metrics_name]
-#             self.metrics = self.new_val_name+self.metrics_name
             if self.batch_num%self.eval_batch_num==0:
                 loss_list = self.model.evaluate(x=self.valid_x, y=self.valid_y, batch_size=self.valid_batch_size, verbose=0, sample_weight=self.valid_sample_weight, steps=self.valid_steps)
                 for loss_value, new_name in zip(loss_list, self.new_val_name):
                     logs[new_name] = loss_value
-            else:
-                for new_name in self.new_val_name:
-                    logs[new_name] = None
-#         if self.figsize is None:
-#             self.figsize = (self.columns*self.cell_size[0], ((len(self.metrics)+1)//self.columns+1)*self.cell_size[1])
         for metric in logs:
             self.batch_logs[metric] += [logs[metric]]
         self.draw(metrics=self.metrics, logs=self.batch_logs, batch=self.batch_num, columns=self.columns,
@@ -96,8 +113,7 @@ class PlotMetricsOnBatch(Callback):
                   figsize=self.figsize, cell_size=self.cell_size, valid_fmt=self.valid_fmt)
     
     def draw(self, metrics, logs, batch, columns, iter_num, wait_num, eval_batch_num, figsize, cell_size, valid_fmt):
-        logit = batch%wait_num==0 #if eval_batch_num is None else (batch%wait_num==0)|(batch%eval_batch_num==0)
-        if logit:
+        if batch%wait_num==0:
             clear_output(wait=True)
             plt.figure(figsize=figsize)
             for metric_id, metric in enumerate(metrics):
@@ -106,14 +122,8 @@ class PlotMetricsOnBatch(Callback):
                     plt.xlim(1, iter_num)
                 plt.plot(range(1, len(logs[metric])+1), logs[metric], label="train")
                 if valid_fmt.format(metric) in logs:
-                    plt.plot(range(1, len(logs[metric])+1), logs[valid_fmt.format(metric)], label=valid_fmt.split('_')[0])
-                
-                
-#                 if valid_fmt.split('_')[0] not in metric:
-#                     plt.plot(range(1, len(logs[metric])+1), logs[metric], label="train")
-#                 else:
-#                     plt.plot(range(eval_batch_num, len(logs[metric])*eval_batch_num+1, eval_batch_num),
-#                              logs[metric], label=valid_fmt.split('_')[0])
+                    plt.plot(range(eval_batch_num, len(logs[valid_fmt.format(metric)])*eval_batch_num+1, eval_batch_num),
+                             logs[valid_fmt.format(metric)], label=valid_fmt.split('_')[0])
                 plt.title(metric)
                 plt.xlabel('batch')
                 plt.legend(loc='center right')
