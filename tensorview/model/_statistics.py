c8c3c64f8b9949492fecfbc615b29702a5bf31bf
JinQing Lee <hourout@163.com>
2019-09-09 14:49:32 +0800

    Update _statistics.py

--- tensorview/model/_statistics.py
+++ tensorview/model/_statistics.py
@@ -4,7 +4,7 @@ from pyecharts.options import ComponentTitleOpts
 
 __all__ = ['statistics']
 
-def statistics(model, jupyter=True, path='Kolmogorov-Smirnov Curve.html', title="Model Summary", subtitle=""):
+def statistics(model, jupyter=True, path='Model Summary.html', title="Model Summary", subtitle=""):
     t = pd.DataFrame([[i.name, i.__class__.__name__, i.trainable, i.dtype, i.input_shape, i.output_shape, i.count_params()] for i in model.layers],
                      columns=['layer_custom_name', 'layer_object_name', 'trainable', 'dtype', 'input_shape', 'output_shape', 'params'])
 #     t['output_memory(MB)'] = (t.output_shape.map(lambda x:sum([reduce(lambda y,z:y*z, i[1:]) for i in x]) if isinstance(x, list) else reduce(lambda y,z:y*z, x[1:]))
